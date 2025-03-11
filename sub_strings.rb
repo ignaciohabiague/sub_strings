59d4273543fcf2557866942d02a063e496bd9183
Ignacio <ignacio.habiague1@gmail.com>
2025-03-11 14:31:36 -0300

    main function "substrings" added

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,16 +1,5 @@
 def substrings(word, dictionary)
-    word = word.downcase  # Convertimos el texto a minúsculas
-    result = Hash.new(0)  # Creamos un hash con valor por defecto 0
-  
-    dictionary.each do |substring|
-      count = word.scan(substring).length  # Contamos las apariciones de la subcadena
-      result[substring] = count if count > 0  # Guardamos solo si aparece al menos una vez
-    end
-  
-    result
-  end
-  
-  dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-  
-  puts substrings("Howdy partner, sit down! How's it going?", dictionary)
-  
+    word = word.downcase  # Converts text to downcase
+    result = Hash.new(0)  # Creates hash (default value = 0)
+end
+
