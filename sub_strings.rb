f8260c897a229faafe37c16e1e1952bf691dbe53
Ignacio <ignacio.habiague1@gmail.com>
2025-03-11 14:34:05 -0300

    loop that iterates dictionary added

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,5 +1,12 @@
 def substrings(word, dictionary)
     word = word.downcase  # Converts text to downcase
     result = Hash.new(0)  # Creates hash (default value = 0)
+
+    dictionary.each do |substring|
+      count = word.scan(substring).length  # Counts number of appearances
+      result[substring] = count if count > 0  # Saves it ONLY IF it appears
+    end
+  
+    result
 end
 
